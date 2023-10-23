42f98d7e1e1d4efe52cb52c285c5534859a2cc72
taotechs <taofiqakintunde@gmail.com>
2023-10-23 20:24:30 +0200

    more Func: UI

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreenPage extends StatelessWidget {
@@ -7,37 +9,121 @@ class WeatherScreenPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Weather App"),
+        title: const Text(
+          "Weather App",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
-        actions: const [Icon(Icons.replay_outlined)],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    spreadRadius: 3,
-                    blurRadius: 7,
-                    offset: Offset(3, 3),
-                  ),
-                ],
-                color: Colors.black54,
-                borderRadius: BorderRadius.all(Radius.circular(10))),
-            height: 150,
-            width: 350,
-            child: const Center(child: Text(" Weather")),
-          ),
-          Container(
-            color: Colors.black45,
-          ),
-          Container(
-            color: Colors.black45,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.replay_outlined),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                elevation: 10,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "300° F",
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 10),
+                          Icon(
+                            Icons.cloud_sharp,
+                            size: 64,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Rain",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Weather Forecast",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 15),
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Additional information",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        padding: const EdgeInsets.all(8),
+        child: const Column(
+          children: [
+            Text(
+              "12:00",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Icon(
+              Icons.cloud,
+              size: 32,
+            ),
+            SizedBox(height: 8),
+            Text("33333")
+          ],
+        ),
       ),
     );
   }
